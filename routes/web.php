083987b9bf1a85aad35ca96f3f083b42947908a6
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-07 13:08:10 -0300

    criação das rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sobre', function () {
+    return view('sobre');
+});
+Route::get('/alunos', function () {
+    return view('alunos');
+});
+Route::get('/contato', function () {
+    return view('contato');
+});
