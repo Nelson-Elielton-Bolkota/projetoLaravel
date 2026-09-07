f2f91431a59fd78b592386dec863868970694fe4
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-07 16:03:34 -0300

    desafio

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,6 @@
+<nav>
+    <a href="{{ url('/') }}">Home</a>
+    <a href="{{ url('/alunos') }}">Alunos</a>
+    <a href="{{ url('/sobre') }}">Sobre</a>
+    <a href="{{ url('/contato') }}">Contato</a>
+</nav>
