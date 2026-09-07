c72a59f5d7518cc745214caec980c7d1a942f68e
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-07 19:20:27 -0300

    adição do breeze

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
-use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/sobre', function () {
-    return "sobre";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/contato', function () {
-    return "contato";
-});
-
-// Rotas de Exemplo / Parâmetros
-Route::get('/produto/{id}', function ($id) {
-    return "Exibindo o produto de ID: " . $id;
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Exibindo a categoria de ID: " . $id;
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Exibindo o usuário de ID: " . $id;
-});
-
-Route::resource('alunos', AlunoController::class);
+require __DIR__.'/auth.php';
