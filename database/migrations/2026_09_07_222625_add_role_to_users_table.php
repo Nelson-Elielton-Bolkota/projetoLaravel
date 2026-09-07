585708e2c55a2e60137b635cc84f28adf7492375
Nelson Elielton Bolkota <bolkotanelson@gmail.com>
2026-09-07 20:59:22 -0300

    Adiciona roles ao usuário

--- database/migrations/2026_09_07_222625_add_role_to_users_table.php
+++ database/migrations/2026_09_07_222625_add_role_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-    $table->string('role')->default('aluno');
-    });
+            $table->string('role')->default('professor');
+        });
     }
 
     /**
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 };
